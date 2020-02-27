00f90e58ba390ae60cf794c9c0b765639f082a20
mllim <mlim@fb.com>
2020-02-27 00:05:04 -0800

    Add some basic cpplearning code

---
+++ cpplearning/Params.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+
+class TestClass {
+public:
+  // Default constructor
+  TestClass() { std::cout << "Default Constructor: TestClass()" << std::endl; }
+
+  // One arg constructor
+  explicit TestClass(int a) : a_(a) {
+    std::cout << "One Arg Constructor: TestClass(int a)" << std::endl;
+  }
+
+  // Destructor
+  ~TestClass() { std::cout << "Destructor: ~TestClass()" << std::endl; }
+
+  // Copy constructor
+  TestClass(const TestClass &t) {
+    std::cout << "Copy Constructor: TestClass(const TestClass &t)" << std::endl;
+    a_ = t.a_;
+  }
+
+  // Move constructor
+  TestClass(const TestClass &&t) {
+    std::cout << "Move Constructor: TestClass(const TestClass &&t)"
+              << std::endl;
+    a_ = t.a_;
+  }
+
+  // Copy assignment
+  TestClass &operator=(const TestClass &t) {
+    std::cout << "Copy Assignment: TestClass &operator=(const TestClass &t)"
+              << std::endl;
+    a_ = t.a_;
+    return *this;
+  }
+
+  // Move assignment
+  TestClass &operator=(const TestClass &&t) {
+    std::cout << "Move Assignment: TestClass &operator=(const TestClass &&t)"
+              << std::endl;
+    a_ = t.a_;
+    return *this;
+  }
+
+  int a() const { return a_; }
+
+private:
+  int a_ = 5;
+};
+
+void useAll() {
+  // Default constructor
+  TestClass t1;
+
+  // One arg constructor
+  TestClass t2(10);
+
+  // Copy constructor
+  TestClass t3 = t2;
+  TestClass t4(t2);
+
+  // Move constructor
+  TestClass t5 = std::move(t1);
+  TestClass t6 = std::move(t2);
+
+  // Copy assignment
+  t3 = t2;
+
+  // Move assignment
+  t4 = std::move(t2);
+}
+
+class TestClass2 {
+public:
+  TestClass2(TestClass t) : t1(std::move(t)) {
+    std::cout << "TestClass2(TestClass t)" << std::endl;
+  }
+
+  TestClass t1;
+};
+
+int main() {
+  //   useAll();
+  TestClass t1;
+  TestClass2 t2(t1);
+}
